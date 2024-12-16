96143a7189134cac2b0529eac900a9c17b7ed0b9
mavlekiev <mavlekiev@gmail.com>
2024-12-17 05:05:20 +0700

    fix(basic-3.7): add semicolon after properties

--- app.js
+++ app.js
@@ -139,13 +139,13 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
 
 //cycle over incompleteTaskHolder ul list items
 //for each list item
-for (var i=0; i<incompleteTaskHolder.children.length; i++) {
+for (var i = 0; i< incompleteTaskHolder.children.length; i++) {
   //bind events to list items chldren(tasksCompleted)
   bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
 }
 
 //cycle over completedTasksHolder ul list items
-for (var i=0; i<completedTasksHolder.children.length; i++) {
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
   //bind events to list items chldren(tasksIncompleted)
   bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
 }
